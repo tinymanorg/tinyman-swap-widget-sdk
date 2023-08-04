918ea84a661f83cabad6c293b210fc3545c06f2e
Cem KIRAY <jamcry@hotmail.com>
2023-08-04 13:52:07 +0200

    fix: bundling issues

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import {terser} from "rollup-plugin-terser";
+import json from "@rollup/plugin-json";
+import {babel} from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist",
+      format: "cjs"
+    },
+    plugins: [
+      resolve({extensions: [".js", ".jsx", ".ts", ".tsx"]}),
+      commonjs(),
+      babel({
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        babelHelpers: "bundled",
+        include: ["src/**/*"]
+      }),
+      terser(),
+      json()
+    ],
+    external: ["algosdk", "@tinymanorg/tinyman-js-sdk"]
+  }
+];
