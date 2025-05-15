83b6e2f257b61084b9039b670b9f28bed0d9ffa9
gulcinuras <gulcinuras@sabanciuniv.edu>
2025-05-15 18:05:02 +0200

    fix(dist): Remove dist folder

--- dist/constants.d.ts
+++
@@ -1,4 +0,0 @@
-export declare enum NetworkToggleValue {
-    MainNet = "mainnet",
-    TestNet = "testnet"
-}
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./WidgetController.types";
-export { WidgetController } from "./WidgetController";
